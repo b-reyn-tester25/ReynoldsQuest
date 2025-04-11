3614d278a420e764ee3cb4eced17088ca5e60591
Brandon Reynolds <brandonreynolds@Brandons-MacBook-Air.local>
2025-04-11 14:53:19 -0500

    ✅ Add service worker for PWA support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import React from "react";
-import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+serviceWorkerRegistration.register();
